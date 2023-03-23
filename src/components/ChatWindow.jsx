574484302684d6d79c10cdc6085d3e340c64fc37
Maneesh Cheema <ayechico21@gmail.com>
2023-03-23 23:26:45 +0530

    done simple realtime chat app

--- src/components/ChatWindow.jsx
+++ src/components/ChatWindow.jsx
@@ -3,7 +3,9 @@ import ChatScreen from "./ChatScreen";
 import InputBox from "./InputBox";
 import { supabase } from "../utilities";
 function ChatWindow() {
-  const [messages, setMessages] = React.useState(null);
+  const [messages, setMessages] = React.useState(null); // store chat messages
+
+  /*Subscribe to real time API which triggers on db insert on table */
   const subscription = supabase
     .channel("any")
     .on(
@@ -12,11 +14,12 @@ function ChatWindow() {
       (message) => {
         console.log("Change received!", message);
         const nextMessages = [...messages, message.new];
-        setMessages((prev) => nextMessages);
+        setMessages((prev) => nextMessages); // update messages in react
       }
     )
     .subscribe();
   React.useEffect(() => {
+    /*Fetch messages from supabase db */
     async function getAllMessages() {
       const { data, error } = await supabase.from("messages").select();
       setMessages(data);
--- src/components/InputBox.jsx
+++ src/components/InputBox.jsx
@@ -1,19 +1,27 @@
 import React from "react";
 import { supabase } from "../utilities";
 
+/*Insert data into supabase db */
 async function handleSend(data) {
   const message = { content: data };
   const { error } = await supabase.from("messages").insert(message);
 }
 
 function InputBox() {
-  const [input, setInput] = React.useState("");
+  const [input, setInput] = React.useState(""); // track input message
   return (
     <>
       <input
         type="text"
         value={input}
         onChange={(event) => setInput(event.target.value)}
+        onKeyDown={(event) => {
+          /*when user hits enter key => send message */
+          if (event.key === "Enter") {
+            handleSend(input);
+            setInput("");
+          }
+        }}
       ></input>
       <button
         onClick={() => {
