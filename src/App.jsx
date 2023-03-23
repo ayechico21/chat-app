ae2592e70548bfe1fc1d1d13f6b6e7e8cca5cd4b
Maneesh Cheema <ayechico21@gmail.com>
2023-03-23 16:16:07 +0530

    added read and write data utility

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,6 @@ import { _SUPABASE_KEY, _SUPABASE_URL } from "./utilities";
 
 function App() {
   const supabase = createClient(_SUPABASE_URL, _SUPABASE_KEY);
-  console.log(supabase);
-
   return <div className="App"></div>;
 }
 
