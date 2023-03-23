92379638ccb699ea46ef7778446706caee4b61e4
Maneesh Cheema <ayechico21@gmail.com>
2023-03-23 15:54:25 +0530

    added supabase client

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import { createClient } from "@supabase/supabase-js";
+import { _SUPABASE_KEY, _SUPABASE_URL } from "./utilities";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const supabase = createClient(_SUPABASE_URL, _SUPABASE_KEY);
+  console.log(supabase);
 
-  return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+  return <div className="App"></div>;
 }
 
-export default App
+export default App;
---
+++ src/utilities.js
@@ -0,0 +1,3 @@
+export const _SUPABASE_URL = "https://befufldoxnoflkexrskn.supabase.co";
+export const _SUPABASE_KEY =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJlZnVmbGRveG5vZmxrZXhyc2tuIiwicm9sZSI6ImFub24iLCJpYXQiOjE2Nzk1NTkyODcsImV4cCI6MTk5NTEzNTI4N30.j5pQ9CipYoQgNkOuUWVsZ9wLRU2qJyQVP9rEOsYmkUg";
